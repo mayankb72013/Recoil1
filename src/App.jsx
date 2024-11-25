681b62712619276f2b70117b6fe1a2ec1665b108
Mayank Balmoori <balmoorimayank@gmail.com>
2024-11-25 20:07:37 +0530

    Added Recoil to the node modules and made a very basic recoil counter application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
+import { CounterValue } from "./store/atoms/atom"
+
 function App(){
     return(
         <>
+           <RecoilRoot>
+              <Counter></Counter>
+              <Increase></Increase>
+              <Decrease></Decrease>
+           </RecoilRoot>
         </>
     )
+}
+function Counter(){
+    const count = useRecoilValue(CounterValue);
+    return <div>{count}</div>
+}
+function Increase(){
+    const setCount = useSetRecoilState(CounterValue);
+    
+    function inc(){
+        setCount(c=>c+1);
+    }
+
+    return(
+        <button onClick={inc}>Increase</button>
+    )
+}
+function Decrease(){
+   const setCount = useSetRecoilState(CounterValue);
+    
+   function dec(){
+       setCount(c=>c-1);
+   }
+
+   return(
+       <button onClick={dec}>Decrease</button>
+   )
 }
 export default App
